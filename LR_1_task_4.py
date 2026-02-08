e917a0434d1d16db00854a16aecd4e3da91197e6
Piadmy <103594350+Piadmy@users.noreply.github.com>
2026-02-08 03:37:00 +0100

    Update LR_1_task_4.py

--- LR_1_task_4.py
+++ LR_1_task_4.py
@@ -195,3 +195,100 @@ print("Accuracy of the second run =", round(accuracy_test2, 2), "%")
 
 # Візуалізація результатів на тестових даних
 visualize_classifier(classifier2, X_test2, y_test2)
+
+
+#1_4_v2
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split, cross_val_score
+
+def visualize_classifier(classifier, X, y):
+    # Define the minimum and maximum values for X and Y
+    # that will be used in the mesh grid
+    min_x, max_x = X[:, 0].min() - 1.0, X[:, 0].max() + 1.0
+    min_y, max_y = X[:, 1].min() - 1.0, X[:, 1].max() + 1.0
+
+    # Define the step size to use in plotting the mesh grid
+    mesh_step_size = 0.01
+
+    # Define the mesh grid of X and Y values
+    x_vals, y_vals = np.meshgrid(np.arange(min_x, max_x, mesh_step_size), np.arange(min_y, max_y, mesh_step_size))
+
+    # Run the classifier on the mesh grid
+    output = classifier.predict(np.c_[x_vals.ravel(), y_vals.ravel()])
+
+    # Reshape the output array
+    output = output.reshape(x_vals.shape)
+
+    # Create a plot
+    plt.figure()
+
+    # Choose a color scheme for the plot
+    plt.pcolormesh(x_vals, y_vals, output, cmap=plt.cm.gray)
+
+    # Overlay the training points on the plot
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=75, edgecolors='black', linewidth=1, cmap=plt.cm.Paired)
+
+    # Specify the boundaries of the plot
+    plt.xlim(x_vals.min(), x_vals.max())
+    plt.ylim(y_vals.min(), y_vals.max())
+
+    # Specify the ticks on the X and Y axes
+    plt.xticks((np.arange(int(X[:, 0].min() - 1), int(X[:, 0].max() + 1), 1.0)))
+    plt.yticks((np.arange(int(X[:, 1].min() - 1), int(X[:, 1].max() + 1), 1.0)))
+
+    plt.show()
+
+# Input file containing data
+input_file = 'data_multivar_nb.txt'
+
+# Load data from input file
+data = np.loadtxt(input_file, delimiter=',')
+X, y = data[:, :-1], data[:, -1]
+
+# Create Naive Bayes classifier
+classifier = GaussianNB()
+classifier.fit(X, y)
+
+# Predict on training data
+y_pred = classifier.predict(X)
+
+# Accuracy on training data
+accuracy = 100.0 * (y == y_pred).sum() / X.shape[0]
+print("Accuracy of Naive Bayes classifier =", round(accuracy, 2), "%")
+
+# Visualize classifier performance (only if 2D)
+if X.shape[1] == 2:
+    visualize_classifier(classifier, X, y)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=3
+)
+
+classifier_new = GaussianNB()
+classifier_new.fit(X_train, y_train)
+
+y_test_pred = classifier_new.predict(X_test)
+
+accuracy_test = 100.0 * (y_test == y_test_pred).sum() / X_test.shape[0]
+print("Accuracy of the new classifier =", round(accuracy_test, 2), "%")
+
+if X.shape[1] == 2:
+    visualize_classifier(classifier_new, X_test, y_test)
+
+
+num_folds = 3
+
+accuracy_values = cross_val_score(classifier, X, y, scoring="accuracy", cv=num_folds)
+print("Accuracy: " + str(round(100 * accuracy_values.mean(), 2)) + "%")
+
+precision_values = cross_val_score(classifier, X, y, scoring="precision_weighted", cv=num_folds)
+print("Precision: " + str(round(100 * precision_values.mean(), 2)) + "%")
+
+recall_values = cross_val_score(classifier, X, y, scoring="recall_weighted", cv=num_folds)
+print("Recall: " + str(round(100 * recall_values.mean(), 2)) + "%")
+
+f1_values = cross_val_score(classifier, X, y, scoring="f1_weighted", cv=num_folds)
+print("F1: " + str(round(100 * f1_values.mean(), 2)) + "%")
+
